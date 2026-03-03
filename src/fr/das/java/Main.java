f3b363b514028ea789a77c22a76965b80426e041
suFredericD <ilric6666@gmail.com>
2026-03-03 17:38:47 +0100

    On branch Interface  Your branch is up to date with 'DAS/Interface'.
     Changes to be committed:
    	modified:   src/fr/das/java/Main.java
    
        Modification du Main pour lancement de l'écran d'accueil.
        Création de listes pour les factions et les lieux.

--- src/fr/das/java/Main.java
+++ src/fr/das/java/Main.java
@@ -1,7 +1,97 @@
 package fr.das.java;
 
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        public static void main(String[] args) {
+                // Génération des éléments
+                List<Faction> factions = generateFactions();
+                List<Location> locations = generateLocations(factions);
+
+                // Génération du joueur
+                Player player = new Player("Ilric", "Haune", factions.get(3), 1, 100, 100, 100, 100, 1000, 20, 20, 20,
+                                20, 20,
+                                10,
+                                100, 100);
+
+                House playerHouse = new House("Maison du joueur",
+                                "Ta maison personnelle, un refuge sûr et un symbole de ton statut sur Arrakis.", player,
+                                locations.get(1));
+
+                // Attribution des éléments du jeu
+                player.setHome(playerHouse);
+
+                // Génération de la partie
+
+                // Page d'accueil de la console
+                clearConsole(); // Réinitialisation de la console
+                System.out.println(ConsoleColors.YELLOW + "====================================================");
+                System.out.println("DUNE : ARRAKIS SMUGGLER");
+                System.out.println("====================================================" + ConsoleColors.RESET);
+                new ScreenWelcome(player);
+
+                // Retours en console
+
+                // Fin d'affichage console
+                System.out.println(
+                                ConsoleColors.YELLOW + "\n_____________________________________ fin d'éxécution\n"
+                                                + ConsoleColors.RESET);
+        }
+        // ==============================================================================
+        // =================================================================== FONCTIONS
+        // ==============================================================================
+
+        // ===================================================================
+        // Génération des éléments
+        public static List<Faction> generateFactions() {
+                Faction atreides = new Faction("Atréides",
+                                "La maison noble des Atreides, connue pour son honneur, sa loyauté et sa sagesse.",
+                                "Maison majeure");
+                Faction harkonnen = new Faction("Harkonnen",
+                                "La maison rivale des Atréides, caractérisée par sa cruauté, sa soif de pouvoir et son règne de terreur.",
+                                "Maison majeure");
+                Faction fremen = new Faction("Fremen",
+                                "Les habitants indigènes d'Arrakis, des guerriers redoutables vivant dans les sietchs et maîtrisant les secrets du désert.",
+                                "Peuple indigène");
+                Faction smugglers = new Faction("Contrebandiers",
+                                "Les contrebandiers opérant sur Arrakis, spécialisés dans le commerce illégal d'épice et d'autres biens.",
+                                "Faction planétaire");
+                List<Faction> factions = List.of(atreides, harkonnen, fremen, smugglers); // crée une liste immuable
+
+                return factions;
+        }
+
+        public static List<Location> generateLocations(List<Faction> factions) {
+                Location tabr = new Location("Sietch Tabr",
+                                "Un sietch situé dans les montagnes, connu pour son chef Stilgar et son marché noir florissant.",
+                                factions.get(2), 1237, "Sietch");
+                Location arrakeen = new Location("Arrakeen",
+                                "La capitale atreide d'Arrakis, un centre de pouvoir et de commerce, avec des fortifications imposantes et une population cosmopolite.",
+                                factions.get(0), 1789, "Ville");
+                Location neocarthag = new Location("Neo-Carthag",
+                                "La ville harkonnen construite comme base de repli stratégique, connue pour ses industries et son énergie.",
+                                factions.get(1), 1842, "Ville");
+                List<Location> locations = List.of(tabr, arrakeen, neocarthag);
+                return locations;
+        }
+
+        // ======================================= OUTILS
+        // Astuce pour réinitialiser l'affichage de la console
+        public static void clearConsole() {
+                try {
+                        if (System.getProperty("os.name").contains("Windows")) {
+                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                        } else {
+                                System.out.print("\033[H\033[2J");
+                                System.out.flush();
+                        }
+                } catch (Exception e) {
+                        System.out.println("Error clearing console: " + e.getMessage());
+                }
+        }
 }
